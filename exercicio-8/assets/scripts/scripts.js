da13864b5b01452991b21844717079159b17187a
Ovidio Rey <06designer@gmail.com>
2026-05-28 17:23:48 +0200

    maquetacion desde opencode

--- exercicio-8/assets/scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-const botonMenu = document.querySelector("#boton-menu");
-        const menu = document.querySelector("#menu");
-
-        botonMenu.addEventListener("click", () => {
-            menu.classList.toggle("mostrar");
-        });
